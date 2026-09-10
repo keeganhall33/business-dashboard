84de74af70d0fb48d620aa213fb9f515a7e59832
keeganhall33 <keeganhall@gmail.com>
2026-09-10 04:26:09 -0700

    Fix React import in discovery truth tests

--- test/discovery/sports-art-partner-production-truth-v1.test.tsx
+++ test/discovery/sports-art-partner-production-truth-v1.test.tsx
@@ -2,6 +2,7 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
+import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 
 import { SportsArtPartnerIntelligence } from "@/components/discovery/SportsArtPartnerIntelligence";
